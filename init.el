8726d9d0a2753261e9aa4662861c52d57f901377
mohsenil85 <mohsenil85@gmail.com>
2015-10-08 17:43:39 -0700

    more

--- init.el
+++ init.el
@@ -3,14 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'()
- )
+ '(custom-safe-themes
+   (quote
+    ("a33ccce31f398f812561d4b0a9f7a2b7038197b722abc278f50d80f3566a0fdf" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'()
  )
 (require 'ob-tangle)
 (setq debug-on-error t)
