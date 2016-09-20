5baa3c284b83088c3998479b53468d288f27d01f
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2016-09-20 16:22:20 -0700

    reorg

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (load-file custom-file )
 (load-file "~/.emacs.d/recentf" )
 (require 'ob-tangle)
-(setq debug-on-error t)
+;;(setq debug-on-error t)
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
                    user-emacs-directory))
