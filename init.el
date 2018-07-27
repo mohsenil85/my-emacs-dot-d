39a9eebe42b6540fdb813222464cef549ec50dfd
lmohseni <mohsenil85@gmail.com>
2018-07-26 19:59:43 -0600

    add more

--- init.el
+++ init.el
@@ -1,14 +1,6 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
+(package-initialize)
 
-;(add-to-list 'load-path "~/.emacs.d/vendor/benchmark-init-el")
-;(require 'benchmark-init-loaddefs)
-;(benchmark-init/activate)
-
-
+(setq custom-file (make-temp-file "~/.emacs.d/custom"))
 
 (setq package-enable-at-startup nil)
 (setq inhibit-startup-buffer-menu t)
@@ -18,6 +10,8 @@
 (setq initial-scratch-message ":D")
 (setq load-prefer-newer t)
 (setq visible-bell t)
+; (setq mouse-autoselect-window t
+ ;     focus-follows-mouse t  )
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
@@ -27,7 +21,6 @@
 			 ("melpa"     . "https://melpa.org/packages/")
 			 ("org"       . "http://orgmode.org/elpa/")
 			 ))
-(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -37,14 +30,14 @@
 ;;;(setq use-package-enable-imenu-support t)
 (eval-when-compile
   (require 'use-package))
-;(setq use-package-always-defer t)
+					;(setq use-package-always-defer t)
 (require 'diminish)
 (require 'bind-key)
 
 (bind-key "C-c h b" #'describe-personal-keybindings)
 
 
-;(use-package no-littering :ensure t)
+					;(use-package no-littering :ensure t)
 (use-package evil :ensure t :config (evil-mode 1))
 (use-package org :ensure org-plus-contrib)
 
@@ -53,37 +46,16 @@
 
 (require 'ob-tangle)
 
-;;(setq custom-file "~/.emacs.d/customizations.el")
-;;(load-file custom-file)
-;;(load-file "~/.emacs.d/recentf" )
-
-
-
-(setq custom-file "~/.emacs.d/customizations.el")
-(load-file custom-file)
-(load-file "~/.emacs.d/recentf" )
-
-
-
 ;(setq debug-on-error t)
+
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
-                   user-emacs-directory))
+		   user-emacs-directory))
 (put 'narrow-to-page 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("065efdd71e6d1502877fd5621b984cded01717930639ded0e569e1724d058af8" "c620ce43a0b430dcc1b06850e0a84df4ae5141d698d71e17de85e7494377fd81" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "acad73ae74b280b29809d10cda2e8f9015cfb007a00fadee55b85861c7c5b99e" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "c5a886cc9044d8e6690a60f33db45506221aa0777a82ad1f7fe11a96d203fa44" "cab317d0125d7aab145bc7ee03a1e16804d5abdfa2aa8738198ac30dc5f7b569" "7e376fb329a0e46a04e8285b0e45199a083f98c69b0e1039ec1cb1d366e66e9c" default)))
- '(package-selected-packages
-   (quote
-    (font-utils xresources-theme atom-one-dark-theme slime solarized-theme boron-theme minimal-theme hydandata-light-theme monochrome-theme exwm-x image+ exwm evil-magit dired-k soft-morning-theme tide ob-ipython jedi js2-mode rainbow-delimiters paredit meghanada cider clojure-mode dired+ dumb-jump which-key sentence-navigation recursive-narrow projectile persistent-scratch paradox keyfreq page-break-lines use-package org-plus-contrib markdown-mode magit helm-gtags helm-descbinds helm-ag helm-ack git-gutter-fringe general flycheck evil emojify elfeed-org elfeed-goodies diminish bind-map better-defaults auctex ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;(require 'exwm)
+;;(require 'exwm-config)
+;;(exwm-config-default)
+;;(require 'exwm-systemtray)
+;;(exwm-systemtray-enable)
+
