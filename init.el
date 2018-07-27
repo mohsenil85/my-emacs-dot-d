1b7e460595846be42a80bc93774abc8f2235bab4
lmohseni <mohsenil85@gmail.com>
2018-07-27 16:02:36 -0600

    add plenty

--- init.el
+++ init.el
@@ -7,19 +7,6 @@
   )
 
 (setq package-enable-at-startup nil)
-(setq inhibit-startup-buffer-menu t)
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-echo-area-message "lmohseni")
-(setq initial-buffer-choice t)
-(setq initial-scratch-message ":D")
-(setq load-prefer-newer t)
-(setq visible-bell t)
-; (setq mouse-autoselect-window t
- ;     focus-follows-mouse t  )
-(scroll-bar-mode 0)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-
 
 (setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
 			 ("melpa"     . "https://melpa.org/packages/")
@@ -31,22 +18,12 @@
   (package-install 'use-package))
 
 
-;;;(setq use-package-enable-imenu-support t)
 (eval-when-compile
   (require 'use-package))
-					;(setq use-package-always-defer t)
-;(require 'diminish)
-;(require 'bind-key)
 
-;(bind-key "C-c h b" #'describe-personal-keybindings)
-
-
-					;(use-package no-littering :ensure t)
-(use-package evil :ensure t :config (evil-mode 1))
+;;(use-package no-littering :ensure t)
+;;(use-package evil :ensure t :config (evil-mode 1))
 (use-package org :ensure org-plus-contrib)
-
-;(use-package better-defaults :ensure t :defer t )
-
 
 (require 'ob-tangle)
 
@@ -62,4 +39,8 @@
 ;;(exwm-config-default)
 ;;(require 'exwm-systemtray)
 ;;(exwm-systemtray-enable)
+
+; (setq mouse-autoselect-window t
+ ;     focus-follows-mouse t  )
+
 
