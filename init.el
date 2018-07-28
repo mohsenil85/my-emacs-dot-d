7fca9f79e9a9497d3618cd4b60e6d29f9423b904
lmohseni <mohsenil85@gmail.com>
2018-07-27 22:08:35 -0600

    add

--- init.el
+++ init.el
@@ -3,8 +3,7 @@
 (let ((temp-custom (make-temp-file "~/.emacs.d/custom")))
   (setq custom-file temp-custom )
   (add-hook 'kill-emacs-hook
-	    '(lambda () (delete-file custom-file)))
-  )
+	    '(lambda () (delete-file custom-file))))
 
 (setq package-enable-at-startup nil)
 
@@ -16,31 +15,18 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-
 (eval-when-compile
   (require 'use-package))
 
+;;used in debugging
 ;;(use-package no-littering :ensure t)
 ;;(use-package evil :ensure t :config (evil-mode 1))
+;;(setq debug-on-error t)
 (use-package org :ensure org-plus-contrib)
 
 (require 'ob-tangle)
 
-;(setq debug-on-error t)
 
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
 		   user-emacs-directory))
-(put 'narrow-to-page 'disabled nil)
-
-;;(require 'exwm)
-;;(require 'exwm-config)
-;;(exwm-config-default)
-;;(require 'exwm-systemtray)
-;;(exwm-systemtray-enable)
-
-; (setq mouse-autoselect-window t
- ;     focus-follows-mouse t  )
-
-
